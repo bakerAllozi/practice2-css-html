6cbe4dd6995b997c43ba68ab1939307c6e3c31d6
bakerAllozi <160823630+bakerAllozi@users.noreply.github.com>
2024-03-28 17:04:10 +0300

    Add files via upload

---
+++ scrpet.js
@@ -0,0 +1,7 @@
+'use strict';
+const baker1 = document.querySelector('.baker1');
+const tg = document.querySelector('.tg');
+baker1.addEventListener('click', function(){
+    tg.classList.toggle('hidden')
+    
+})
